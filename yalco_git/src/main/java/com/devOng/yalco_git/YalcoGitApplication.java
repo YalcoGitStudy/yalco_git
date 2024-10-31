21d2f4e8f41901ef047855c49c153e81894ffbda
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2024-10-31 11:18:24 +0900

    Add files via upload

---
+++ yalco_git/src/main/java/com/devOng/yalco_git/YalcoGitApplication.java
@@ -0,0 +1,13 @@
+package com.devOng.yalco_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YalcoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YalcoGitApplication.class, args);
+	}
+
+}
---
+++ yalco_git/src/test/java/com/devOng/yalco_git/YalcoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devOng.yalco_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YalcoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
